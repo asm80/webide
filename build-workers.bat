e5555c292a9d744345cc75b7b5c2488da3b9d277
Martin Malý <maly@maly.cz>
2024-03-05 11:51:39 +0100

    empty

--- build-workers.bat
+++
@@ -1,8 +0,0 @@
-set ROOT ./node_modules/monaco-editor/esm/vs
-set OPTS=--no-source-maps --log-level 1
-
-call parcel build ./node_modules/monaco-editor/esm/vs/language/json/json.worker.js --no-source-maps --log-level error
-call parcel build ./node_modules/monaco-editor/esm/vs/language/css/css.worker.js --no-source-maps --log-level error
-call parcel build ./node_modules/monaco-editor/esm/vs/language/html/html.worker.js --no-source-maps --log-level error
-call parcel build ./node_modules/monaco-editor/esm/vs/language/typescript/ts.worker.js --no-source-maps --log-level error
-call parcel build ./node_modules/monaco-editor/esm/vs/editor/editor.worker.js --no-source-maps --log-level error
