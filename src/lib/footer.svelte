7b6fffef77c21faff87a7897c68dae0d12155e17
Martin Malý <maly@maly.cz>
2024-03-06 12:42:56 +0100

    colorize

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,3 +1,3 @@
-<footer class="footer p-0 is-fixed-bottom mt-auto">
+<footer class="footer p-0 is-fixed-bottom mt-auto has-background-dark has-text-light">
     <span class="">Footer</span>
 </footer>
