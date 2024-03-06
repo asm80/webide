a72d37407a678fe7596dcd8cbd45ad06ce3ef692
Martin <maly@maly.cz>
2024-03-06 18:34:45 +0100

    tree fix

--- src/lib/sbtree/sbtree.svelte
+++ src/lib/sbtree/sbtree.svelte
@@ -3,6 +3,14 @@
     export let data;
     export let level=0;
 
+    let options = {
+        expandIcon: 'fa fa-angle-down fa-fw',
+        collapseIcon: 'fa fa-angle-right fa-fw',
+        expandClass: 'show',
+        indent: 1.25,
+        parentsMarginLeft: '1.25rem',
+    }
+
     const showLevel = (level) => {
         for (let item of data) {
             if (item.level === level) {
@@ -10,18 +18,23 @@
             }
         }
     }
+
+    const updown = (item) => {
+        console.log(item)
+        item.expanded = !item.expanded
+        //item=item
+        data=data
+    }
 </script>
 
 <style>
     .sb-tree-wrapper {
-        border: 1px solid #ccc;
-        border-radius: 5px;
     }
     .sb-tree-wrapper div {
-        padding: 5px;
+        /*padding: 5px;*/
     }
     .sb-tree-wrapper div:hover {
-        background-color: #f0f0f0;
+        background-color: #333;
     }
     .sb-tree-wrapper i {
         margin-right: 5px;
@@ -33,9 +46,15 @@
 
 
 {#each data as item, index}
-    <div><i class={item.icon}></i>&nbsp;{item.text} [{index}]</div>
+    <div>
+        {#if item.nodes}
+            <i class={item.expanded?options.expandIcon:options.collapseIcon} on:click={updown(item)}></i>
+        {/if}
+        <i class={item.icon}></i>&nbsp;{item.text}
+        [{index}]
+    </div>
     {#if item.nodes}
-        <div style="margin-left:12px">
+        <div style="margin-left:{options.parentsMarginLeft}">
             <svelte:self data={item.nodes} level={level+1}/>
         </div>
     {/if}
